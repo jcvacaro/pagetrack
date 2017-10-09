1f2ffe467894ec18c1b4cc2c76b9ec167f10a9cf
Juliano Vacaro <juliano.vacaro@hpe.com>
2017-10-09 17:42:48 -0300

    configuring queues using beans

--- apigw/src/main/java/juliano/pagetrack/apigw/APIGWApplication.java
+++ apigw/src/main/java/juliano/pagetrack/apigw/APIGWApplication.java
@@ -1,10 +1,23 @@
 package juliano.pagetrack.apigw;
 
+import juliano.pagetrack.apigw.service.APIGWQueueService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class APIGWApplication {
+
+	@Bean
+	public Queue contactQueue() {
+		return new Queue(APIGWQueueService.CONTACT_QUEUE_NAME);
+	}
+
+	@Bean
+	public Queue pageAccessQueue() {
+		return new Queue(APIGWQueueService.PAGEACCESS_QUEUE_NAME);
+	}
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(APIGWApplication.class, args);
