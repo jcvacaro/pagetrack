97c588d91bf8e0eb9efc567f7165f843dec33200
Juliano Vacaro <juliano.vacaro@hpe.com>
2017-10-10 14:07:19 -0300

    adding logs to CookieHelper class

--- frontend/src/main/java/juliano/pagetrack/frontend/util/CookieHelper.java
+++ frontend/src/main/java/juliano/pagetrack/frontend/util/CookieHelper.java
@@ -5,12 +5,16 @@ import java.rmi.server.UID;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CookieHelper {
 
+	private static final Logger logger = LoggerFactory.getLogger(CookieHelper.class);
 	private static final String COOKIE_NAME = "juliano.pagetrack.id";
 
 	public Cookie createCookie() {
+		logger.info("createCookie invoked");
 		Cookie c = new Cookie(COOKIE_NAME, new UID().toString());
 		c.setMaxAge(Integer.MAX_VALUE);
 		return c;
@@ -19,12 +23,16 @@ public class CookieHelper {
 	public Cookie getCookie(HttpServletRequest request) {
 		Cookie[] cookies = request.getCookies();
 		if ((cookies != null) && (cookies.length > 0)) {
+			logger.info("cookies found in the request: cookies=" + cookies.length);
 			for (Cookie cookie : cookies) {
 				if (cookie.getName().startsWith(COOKIE_NAME)) {
+					logger.info("application cookie found in the request: cookie=" + cookie.getName());
 					return cookie;
 				}
 			}
+			logger.info("application cookie not found");
 		}
+		logger.info("no cookies present in the request");
 		return null;
 	}
 
